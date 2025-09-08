5e4def8db116f8256e0920c449e3dd1aeb2d3572
c0516154 <andy.nenth@gmail.com>
2025-09-07 17:18:58 -0700

    Remove generated static files from git tracking - Remove backend/static/chunks/* from repository - Remove backend/static/index.html from repository - These files are build outputs that change frequently - They should not be version controlled - This will prevent recurring pre-commit failures

--- backend/static/chunks/sessionStorage-BBDYIII3.js
+++
@@ -1,2 +0,0 @@
-import{a,b,c,d,e,f,g,h,i,j}from"./chunk-J67N2V3R.js";import"./chunk-FZAF5VMJ.js";export{d as clearSession,h as formatSessionInfo,j as getMultiSessions,c as getSession,f as getSessionAge,g as getTimeSinceLastActivity,e as hasValidSession,i as storeMultiSession,a as storeSession,b as updateSessionActivity};
-//# sourceMappingURL=sessionStorage-BBDYIII3.js.map
